905ecb245683ffe9ddf344c3d76ce5442536e00b
Obul Reddy Rajula <rajtechnologies.be@gmail.com>
2026-08-11 17:17:25 +0200

    header footer and content changed

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,14 +1,23 @@
-import { Component, inject } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject, signal } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './header.html',
   styleUrl: './header.css',
   standalone: true,
 })
 export class Header {
   readonly auth = inject(AuthService);
+  readonly menuOpen = signal(false);
+
+  toggleMenu(): void {
+    this.menuOpen.update((open) => !open);
+  }
+
+  closeMenu(): void {
+    this.menuOpen.set(false);
+  }
 }
